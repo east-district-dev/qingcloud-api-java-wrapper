205bbd7e16c7f111c803dc8a072345e869b38b1d
liudi <liudi96@126.com>
2020-09-11 17:13:07 +0800

    创建负载均衡请求增加mode属性

--- src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/CreateLoadBalancerRequest.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/CreateLoadBalancerRequest.java
@@ -4,73 +4,90 @@ import java.util.List;
 
 
 public class CreateLoadBalancerRequest extends Request {
-    
-	private List<String> eips;
-	private Integer loadbalancer_type;
-	private String loadbalancer_name;
-	private String security_group;
-	private String zone;
-	private String vxnet;
-	private String private_ip;
+
+    private List<String> eips;
+    private Integer loadbalancer_type;
+    private String loadbalancer_name;
+    private String security_group;
+    private String zone;
+    private String vxnet;
+    private String private_ip;
+    private Integer mode = 1;
+
+    public Integer getLoadbalancer_type() {
+        return loadbalancer_type;
+    }
+
+    public void setLoadbalancer_type(Integer loadbalancer_type) {
+        this.loadbalancer_type = loadbalancer_type;
+    }
+
+    public String getLoadbalancer_name() {
+        return loadbalancer_name;
+    }
+
+    public void setLoadbalancer_name(String loadbalancer_name) {
+        this.loadbalancer_name = loadbalancer_name;
+    }
+
+    public String getSecurity_group() {
+        return security_group;
+    }
+
+    public void setSecurity_group(String security_group) {
+        this.security_group = security_group;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public String getVxnet() {
+        return vxnet;
+    }
+
+    public void setVxnet(String vxnet) {
+        this.vxnet = vxnet;
+    }
 
 
-	public List<String> getEips() {
-		return eips;
-	}
-	public void setEips(List<String> eips) {
-		this.eips = eips;
-	}
-	public Integer getLoadbalancer_type() {
-		return loadbalancer_type;
-	}
-	public void setLoadbalancer_type(Integer loadbalancer_type) {
-		this.loadbalancer_type = loadbalancer_type;
-	}
-	public String getLoadbalancer_name() {
-		return loadbalancer_name;
-	}
-	public void setLoadbalancer_name(String loadbalancer_name) {
-		this.loadbalancer_name = loadbalancer_name;
-	}
-	public String getSecurity_group() {
-		return security_group;
-	}
-	public void setSecurity_group(String security_group) {
-		this.security_group = security_group;
-	}
-	public String getZone() {
-		return zone;
-	}
-	public void setZone(String zone) {
-		this.zone = zone;
-	}
+    @Override
+    public String toString() {
+        return "CreateLoadBalancerRequest{" +
+                ", loadbalancer_type=" + loadbalancer_type +
+                ", loadbalancer_name='" + loadbalancer_name + '\'' +
+                ", security_group='" + security_group + '\'' +
+                ", zone='" + zone + '\'' +
+                ", vxnet='" + vxnet + '\'' +
+                '}';
+    }
 
-	public String getVxnet() {
-		return vxnet;
-	}
+    public String getPrivate_ip() {
+        return private_ip;
+    }
 
-	public void setVxnet(String vxnet) {
-		this.vxnet = vxnet;
-	}
+    public void setPrivate_ip(String private_ip) {
+        this.private_ip = private_ip;
+    }
 
 
-	@Override
-	public String toString() {
-		return "CreateLoadBalancerRequest{" +
-				"eips=" + eips +
-				", loadbalancer_type=" + loadbalancer_type +
-				", loadbalancer_name='" + loadbalancer_name + '\'' +
-				", security_group='" + security_group + '\'' +
-				", zone='" + zone + '\'' +
-				", vxnet='" + vxnet + '\'' +
-				'}';
-	}
+    public Integer getMode() {
+        return mode;
+    }
 
-	public String getPrivate_ip() {
-		return private_ip;
-	}
+    public void setMode(Integer mode) {
+        this.mode = mode;
+    }
 
-	public void setPrivate_ip(String private_ip) {
-		this.private_ip = private_ip;
-	}
+    public List<String> getEips() {
+        return eips;
+    }
+
+    public void setEips(List<String> eips) {
+        this.eips = eips;
+    }
 }
